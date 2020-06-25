097ea5ced85a2ebb7a4bd541878b9a3124ad829f
Laercio-pc <lndsilva@hotmail.com>
2020-06-24 22:28:41 -0300

    fragment

---
+++ app/src/main/java/br/com/local/appbotomnavview/Favorite_Fragment.java
@@ -0,0 +1,16 @@
+package br.com.local.appbotomnavview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class Favorite_Fragment extends FrameLayout {
+
+
+    public Favorite_Fragment(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
