f5b22b3c1fec477c5c80ccd540701d3de35ad425
Laercio-Note <lndsilva@hotmail.com>
2020-11-30 22:21:06 -0300

    criando BottomNav

---
+++ app/src/main/java/br/com/local/appbotomnavview/Mobile_Fragment.java
@@ -0,0 +1,22 @@
+package br.com.local.appbotomnavview;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Mobile_Fragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.mobile_fragment, container, false);
+    }
+}
